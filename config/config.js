680d92b3fe1966a27c66061c8e6039c54603258c
Marina Reichert <shil7en@gmail.com>
2018-12-25 11:54:51 +0100

    added test to config

--- config/config.js
+++ config/config.js
@@ -1,20 +1,31 @@
 const env = process.env.NODE_ENV || "dev";
 // developement configuration object
 const dev = {
-    db: "product_shop_test",
+    db: "product_shop_dev",
     mongoPort: 27017,
     dbUrl: "mongodb://localhost",
+    serverPort: 3000,
     originsWhitelist: [
         'http://localhost:4200',      //CORS white list
     ]
 };
+
+// test configuration object
+const test = {
+    db: "product_shop_test",
+    mongoPort: 27017,
+    dbUrl: "mongodb://localhost",
+    serverPort: 3001
+}
+
 
 //production configuration object
 const prod = {};
 
 const config = {
     dev,
-    prod
+    prod,
+    test
 };
 
 module.exports = config[env];
