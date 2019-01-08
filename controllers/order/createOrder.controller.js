f427cfa4f8f7f907eaf9fea9a9fb299889d413e1
Marina Reichert <shil7en@gmail.com>
2019-01-08 18:24:01 +0100

    resolve eslint errors

--- controllers/order/createOrder.controller.js
+++ controllers/order/createOrder.controller.js
@@ -6,40 +6,38 @@ const _ = require('lodash');
 //Calculate price and post
 module.exports = async function (req, res, next) {
 
-    const newOrder = new orderModel(req.body);
-    if (!_.isUndefined(newOrder)) {
-        if (!_.isUndefined(newOrder.product)) {
-            newOrder.price = 0;
-            let condition = {};
-            for (let i in req.body.product) {
-                condition._id = req.body.product[i].id;
-                let found = await productModel.findById(condition._id, function (err, product) {
-                    if (err) {
-                        let error = new Error();
-                        error.message = 'INTERNAL ERROR OCCURRED';
-                        error.statusCode = 500;
-                        return next(error);
-                    }
-                }).exec();
-                console.log(found);
-                console.log(req.body.product[i]);
-                newOrder.price = newOrder.price + found.unitPrice * newOrder.product[i].amount;
-            }
-        }
-    }
-    newOrder.save(function (err, order) {
-        if (err) {
-            let error = new Error();
-            error.message = 'INTERNAL ERROR OCCURED!';
-            error.statusCode = 500;
-            return next(error);
-        }
+	const newOrder = new orderModel(req.body);
+	if (!_.isUndefined(newOrder)) {
+		if (!_.isUndefined(newOrder.product)) {
+			newOrder.price = 0;
+			let condition = {};
+			for (let i in req.body.product) {
+				condition._id = req.body.product[i].id;
+				let found = await productModel.findById(condition._id, function (err) {
+					if (err) {
+						let error = new Error();
+						error.message = 'INTERNAL ERROR OCCURRED';
+						error.statusCode = 500;
+						return next(error);
+					}
+				}).exec();
+				newOrder.price = newOrder.price + found.unitPrice * newOrder.product[i].amount;
+			}
+		}
+	}
+	newOrder.save(function (err, order) {
+		if (err) {
+			let error = new Error();
+			error.message = 'INTERNAL ERROR OCCURED!';
+			error.statusCode = 500;
+			return next(error);
+		}
 
-        res.status(201).json(
-            {
-                status: 'CREATED',
-                order
-            });
-    });
+		res.status(201).json(
+			{
+				status: 'CREATED',
+				order
+			});
+	});
 
-}
+};
--- models/order.model.js
+++ models/order.model.js
@@ -2,48 +2,42 @@ const mongoose = require('mongoose');
 const Schema  = mongoose.Schema;
 
 const orderSchema = new Schema({
-    product: [{
-        id: {
-            type: String,
-            required: true
-        },
-        amount : {
-            type: Number,
-            min: 1,
-            required: true
-        },
-        _id: false   //to not create _id field for each product     
-    }],
-    paymentMethod: {
-        type: String,
-        enum: ['Paypal', 'Credit card', 'Voucher'],
-        default: 'Paypal'
-    },
-    date: {
-        type: Date,
-        default: Date.now()
-    },
-    orderer: {
-        firstName: {type: String, required: true},
-        lastName: {type: String, required: true}        
-    },
-    adress: {
-        street : {type: String, required: true},    
-        houseNr: {type: String, required: true},
-        zip : {type: Number, required: true},
-        city: {type: String, required: true}
-    },
-    price: {
-        type: Number,
-        required: true
-    }
+	product: [{
+		id: {
+			type: String,
+			required: true
+		},
+		amount : {
+			type: Number,
+			min: 1,
+			required: true
+		},
+		_id: false   //to not create _id field for each product     
+	}],
+	paymentMethod: {
+		type: String,
+		enum: ['PayPal', 'Credit card', 'Voucher'],
+		default: 'PayPal'
+	},
+	date: {
+		type: Date,
+		default: Date.now()
+	},
+	orderer: {
+		firstName: {type: String, required: true},
+		lastName: {type: String, required: true}        
+	},
+	adress: {
+		street : {type: String, required: true},    
+		houseNr: {type: String, required: true},
+		zip : {type: Number, required: true},
+		city: {type: String, required: true}
+	},
+	price: {
+		type: Number,
+		required: true
+	}
 });
-
-orderSchema.methods.setPrice = function (price) {
-    console.log('orderModel ' + price);
-	this.set('price', 1000);
-};
-
 
 module.exports = mongoose.model('Order', orderSchema);
 
